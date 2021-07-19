7575e8f10624a598ed1663d1b6d80985f0bce572
StephenSchmitz123 <stephen.schmitz@method.com>
2021-07-19 14:24:28 -0400

    inital commit

--- ARPlayground/ContentView.swift
+++ ARPlayground/ContentView.swift
@@ -1,35 +1,40 @@
-//
+//.statusBar(hidden: /*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
 //  ContentView.swift
 //  ARPlayground
 //
-//  Created by Stephen Schmitz on 7/14/21.
-//
+//  Created by Stephen Schmitz on 7/14/21.CCA                     //
 
 import SwiftUI
 import RealityKit
 
+
+// main class that displays ARViewContainer and fits full screen
 struct ContentView : View {
     var body: some View {
         return ARViewContainer().edgesIgnoringSafeArea(.all)
     }
 }
 
+// UIView class displays viewable objects
 struct ARViewContainer: UIViewRepresentable {
     
+    // creates our UIView
     func makeUIView(context: Context) -> ARView {
         
+        // makes ARView object
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+
+        // creates Anchor and attaches .rcproject to anchor
+        let anchor = try! ARPlayground.loadScene()
+
+        // appends Anchor to scene (scene automatically created)
+        arView.scene.anchors.append(anchor)
+
+        // return ARView
         return arView
-        
     }
     
+    // updates UIView
     func updateUIView(_ uiView: ARView, context: Context) {}
     
 }
